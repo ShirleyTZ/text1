4e13178387306b7065e767fccb570bb5cc3abf5d
ShirleyTZ <553728720@qq.com>
2017-09-20 21:44:40 +0800

    出现问题

--- text1.js
+++ text1.js
@@ -1,33 +1,34 @@
 function showP(id) {
-    var P = document.getElementsByTagName("p");
-    for (var i=0; i < P.length; i++) {
-    	if (P[i].getAttribute("id") !== id ) {
-    		P[i].style.display = "none";
-    	}else{
-    		P[i].style.display = "block";
-    	}
-    }
+  var article = document.getElementById("article");
+  var DIVS = article.getElementsByClassName("dialog");
+  for (var i=0; i < DIVS.length; i++) {
+   	if (DIVS[i].getAttribute("id") !== id ) {
+      DIVS[i].style.display = "none";
+    }else{
+   	  DIVS[i].style.display = "block";
+   	};
+  };
+};
+function prepare() {
+  var body = document.getElementsByTagName("body");
+  if (body.length == 0) return false;
+  var divs = body[0].getElementsByTagName("div");
+  if (divs.length == 0) return false;
+  var num =divs.length-1;
+  var div = divs[num];
+  var links = div.getElementsByTagName("a");
+  for (var i = 0; i < links.length; i++) {
+	var Pid = links[i].getAttribute("href").split("#")[1];
+	if (!document.getElementById(Pid)) continue;
+	var ps = document.getElementById(Pid) ;		
+	ps.style.display ="none";
+	links[i].destination = Pid;
+	links[i].onclick = function() {
+	  showP(this.destination);
+	  return false;
+	};
+  }
 }
+prepare();
 
-function prepareId() {
-	var body = document.getElementsByTagName("body");
-	if (body.length == 0) return false;
-	var divs = body[0].getElementsByTagName("div");
-	if (divs.length == 0) return false;
-	var num =divs.length-1;
-	var div = divs[num];
-	var links = div.getElementsByTagName("a");
-	for (var i = 0; i < links.length; i++) {
-		var Pid = links[i].getAttribute("href").split("#")[1];
-		if (!document.getElementById(Pid)) continue;
-		var ps = document.getElementById(Pid) ;
-		ps.style.display ="none";
-		links[i].destination = Pid;
-		links[i].onclick = function() {
-			showP(this.destination);
-			return false;
-		}
-	}
-}
 
-window.onload =prepareId;
