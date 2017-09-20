72645927dfce682b3eb40631fa349e1e9521c641
lvwei <lvwei524@gmail.com>
2017-09-20 22:32:23 +0800

    格式修改

--- text1.js
+++ text1.js
@@ -1,32 +1,38 @@
 function showP(id) {
   var article = document.getElementById("article");
-  var DIVS = article.getElementsByClassName("dialog");
-  for (var i=0; i < DIVS.length; i++) {
-   	if (DIVS[i].getAttribute("id") !== id ) {
-      DIVS[i].style.display = "none";
-    }else{
-   	  DIVS[i].style.display = "block";
+  var divs = article.getElementsByClassName("dialog");
+  for (var i = 0; i < divs.length; i++) {
+   	if (divs[i].getAttribute("id") !== id ) {
+      divs[i].style.display = "none";
+    } else {
+   	  divs[i].style.display = "block";
    	};
   };
 };
 function prepare() {
   var body = document.getElementsByTagName("body");
-  if (body.length == 0) return false;
+  if (body.length === 0) {
+    return false;
+  }
   var divs = body[0].getElementsByTagName("div");
-  if (divs.length == 0) return false;
-  var num =divs.length-1;
+  if (divs.length === 0) {
+    return false;
+  }
+  var num = divs.length-1;
   var div = divs[num];
   var links = div.getElementsByTagName("a");
   for (var i = 0; i < links.length; i++) {
-	var Pid = links[i].getAttribute("href").split("#")[1];
-	if (!document.getElementById(Pid)) continue;
-	var ps = document.getElementById(Pid) ;		
-	ps.style.display ="none";
-	links[i].destination = Pid;
-	links[i].onclick = function() {
-	  showP(this.destination);
-	  return false;
-	};
+    var pid = links[i].getAttribute("href").split("#")[1];
+    if (!document.getElementById(pid)) {
+      continue;      
+    }
+    var ps = document.getElementById(pid);		
+    ps.style.display ="none";
+    links[i].destination = pid;
+    links[i].onclick = function() {
+      showP(this.destination);
+      return false;
+    };
   }
 }
 prepare();
